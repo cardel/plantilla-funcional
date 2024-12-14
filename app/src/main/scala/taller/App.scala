5c30203360beca481477c05a1e66c1506cbf1b1a
JuanJoseR18 <juan.restrepo.avalo@correounivalle.edu.co>
2024-12-14 17:00:21 -0500

    Pruebas de tiempo de ejecucion para ProgramacionDeRiegoOptimo

--- app/src/main/scala/taller/App.scala
+++ app/src/main/scala/taller/App.scala
@@ -7,11 +7,12 @@ import scala.util.Random
 
 object App {
   def main(args: Array[String]): Unit = {
-    println(greeting())
+    //println(greeting())
 
     val RiegoOptimo = new RiegoOptimo()
     val longitud_fincas = 10
 
+  //pruebas de tiempo de ejecución para CostoRiegoFinca
     for (i: Int <- 1 to longitud_fincas by 2) yield {
 
       val finca_creada = RiegoOptimo.fincaAlAzar(i)
@@ -30,8 +31,23 @@ object App {
       println(s"Paralelo: $timePar ms")
     }
 
-
+  //pruebas de tiempo de ejecución para ProgramacionRiegoOptimo
+    for(i: Int <- 1 to longitud_fincas by 2) yield {
+      val finca = RiegoOptimo.fincaAlAzar(i)
+      val distancia = RiegoOptimo.distanciaAlAzar(i)
+      val timeSeq = measure {
+        RiegoOptimo.ProgramacionRiegoOptimo(finca, distancia)
+      }
+      val timePar = measure {
+        RiegoOptimo.ProgramacionRiegoOptimoPar(finca, distancia)
+      }
+      println(s"prueba de tieempo de ejecucion con $i tablones:")
+      println(s"Tiempo secuencial: $timeSeq ms")
+      println(s"Tiempo paralelo: $timePar ms")}
   }
 
-  def greeting(): String = "Hello, world!"
 }
+
+
+//def greeting(): String = "Hello, world!"
+
